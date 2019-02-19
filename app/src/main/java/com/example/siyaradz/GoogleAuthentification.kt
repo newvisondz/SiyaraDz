a5d0b3ce391280e0350feb18bd7fb8a7e39d9338
yacinebenkaidali <yacinebenkaidali@gmail.com>
2019-02-19 20:46:49 +0100

    Fixed A Bug & made Code Cleaner

--- app/src/main/java/com/example/siyaradz/GoogleAuthentification.kt
+++ app/src/main/java/com/example/siyaradz/GoogleAuthentification.kt
@@ -1,5 +1,75 @@
 package com.example.siyaradz
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.support.v4.app.FragmentActivity
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInResult
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.GoogleApiClient
+
 class GoogleAuthentification {
-    
+    private var googleClient: GoogleApiClient? = null
+    public val REQ_CODE: Int = 9001
+    private var userName: TextView
+    internal  var signInButton: SignInButton
+    internal  var signOutButton : Button
+
+    constructor(context: Context,signInOptions:GoogleSignInOptions) {
+        context as Activity
+        this.googleClient = GoogleApiClient.Builder(context).enableAutoManage(context as FragmentActivity,context as GoogleApiClient.OnConnectionFailedListener).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build()
+        this.userName = context.findViewById(R.id.userName)
+        this.signInButton = context.findViewById(R.id.googleSignIn)
+        this.signOutButton = context.findViewById(R.id.googleSignOut)
+    }
+
+    fun getgoogleClient(): GoogleApiClient? {
+        return this.googleClient
+    }
+    fun getuserName():TextView {
+        return this.userName
+    }
+
+
+    public fun signIn(context: Context) {
+        var intent: Intent = Auth.GoogleSignInApi.getSignInIntent(googleClient)
+        context as Activity
+        context.startActivityForResult(intent,REQ_CODE)
+    }
+
+    public fun signOut() {
+        Auth.GoogleSignInApi.signOut(googleClient).setResultCallback {
+            updateUi(false)
+        }
+    }
+    public fun handleResult(result: GoogleSignInResult){
+        if (result.isSuccess){
+            var account =result.signInAccount
+            var name = account?.displayName
+            var email =account?.email
+            userName.text = email
+            updateUi(true)
+
+        }else{
+            updateUi(false)
+        }
+
+    }
+
+    public fun  updateUi(isLogedIn:Boolean) {
+        if (isLogedIn) {
+            userName.visibility= View.VISIBLE
+            signInButton.visibility= View.GONE
+
+        }else {
+            userName.visibility= View.GONE
+            signInButton.visibility= View.VISIBLE
+        }
+
+    }
 }
--- app/src/main/java/com/example/siyaradz/MainActivity.kt
+++ app/src/main/java/com/example/siyaradz/MainActivity.kt
@@ -10,9 +10,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import com.bumptech.glide.util.Util
 import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.auth.api.signin.GoogleSignInResult
 import com.google.android.gms.common.ConnectionResult
@@ -23,43 +21,33 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
 
-    private var googleClient: GoogleApiClient? = null
-    private val REQ_CODE: Int = 9001
-    private lateinit var userName: TextView
-    private lateinit var signInButton: SignInButton
-    private lateinit var signOutButton : Button
-
-
+    private var authentification:GoogleAuthentification?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        var signInOptions= GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
-        googleClient =GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build()
-
-        userName = findViewById(R.id.userName)
-        signInButton = findViewById(R.id.googleSignIn)
-        signOutButton=findViewById(R.id.googleSignOut)
+        val signInOptions= GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+        authentification =GoogleAuthentification(this,signInOptions)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        userName.visibility = View.GONE
+        authentification!!.getuserName().visibility = View.GONE
 
 
-        signInButton.setOnClickListener{
-            signIn()
+        authentification!!.signInButton.setOnClickListener{
+            authentification!!.signIn(this)
         }
-        signOutButton.setOnClickListener{
-            signOut()
+        authentification!!.signOutButton.setOnClickListener{
+            authentification!!.signOut()
         }
     }
 
     override fun onConnectionFailed(p0: ConnectionResult) {
-        Toast.makeText(this,"failed",Toast.LENGTH_SHORT)
+        Toast.makeText(this,"Une faible connection internet ",Toast.LENGTH_SHORT)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -76,46 +64,11 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedList
         }
     }
 
-    public fun signIn() {
-       intent=Auth.GoogleSignInApi.getSignInIntent(googleClient)
-        startActivityForResult(intent,REQ_CODE)
-    }
-
-    public fun signOut() {
-        Auth.GoogleSignInApi.signOut(googleClient).setResultCallback {
-            updateUi(false)
-        }
-    }
-    public fun handleResult(result:GoogleSignInResult){
-        if (result.isSuccess){
-            var account =result.signInAccount
-            var name = account?.displayName
-            var email =account?.email
-            userName.text = email
-            updateUi(true)
-
-        }else{
-            updateUi(false)
-        }
-
-    }
-    public fun  updateUi(isLogedIn:Boolean) {
-        if (isLogedIn) {
-            userName.visibility=View.VISIBLE
-            signInButton.visibility=View.GONE
-
-        }else {
-            userName.visibility=View.GONE
-            signInButton.visibility=View.VISIBLE
-        }
-
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode ==REQ_CODE) {
+        if (requestCode ==authentification!!.REQ_CODE) {
             var result =Auth.GoogleSignInApi.getSignInResultFromIntent(data)
-            handleResult(result)
+            this.authentification!!.handleResult(result)
         }
     }
 }
