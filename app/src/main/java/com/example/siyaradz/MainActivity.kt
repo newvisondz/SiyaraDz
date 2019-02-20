20234a6b6cccc9c1a0f5c9cb20082284146ef723
Bara Mohammed Imame <fm_bara@esi.dz>
2019-02-20 22:09:59 +0100

    Initial commit, added facebook login

--- app/src/main/java/com/example/siyaradz/MainActivity.kt
+++ app/src/main/java/com/example/siyaradz/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.siyaradz
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
+import android.widget.TextView
+import com.facebook.CallbackManager
+
+import kotlinx.android.synthetic.main.activity_main.*
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.FacebookCallback
+import com.facebook.login.widget.LoginButton
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var callbackManager = CallbackManager.Factory.create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,24 +29,54 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
-    }
 
-    override fun onPause() {
-        super.onPause()
-        Toast.makeText(this,"",Toast.LENGTH_LONG);
+
+
+        var loginButton = findViewById(R.id.login_button) as LoginButton
+        loginButton.setReadPermissions("email")
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                // App code
+                val builder = AlertDialog.Builder(this@MainActivity)
+                builder.setMessage(if (loginResult.accessToken!=null) "Logged in" else "Login error")
+                val dialog:AlertDialog = builder.create()
+                dialog.show()
+            }
+
+            override fun onCancel() {
+                // App code
+                println("canceled")
+            }
+
+            override fun onError(exception: FacebookException) {
+                // App code
+                println("error")
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
-            else ->{ super.onOptionsItemSelected(item)}
+            else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
