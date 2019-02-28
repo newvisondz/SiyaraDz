64dbd265b07253d273f7c440e2da3f3f5bf437bb
yacinebenkaidali <yacinebenkaidali@gmail.com>
2019-02-28 14:14:15 +0100

    Added GoogleToken And Recycler view with butterKnife

--- app/src/main/java/com/example/siyaradz/BrandDisplay.kt
+++ app/src/main/java/com/example/siyaradz/BrandDisplay.kt
@@ -2,18 +2,30 @@ package com.example.siyaradz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.Nullable
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import butterknife.BindView
 import butterknife.ButterKnife
 
 class BrandDisplay : AppCompatActivity() {
+
     @BindView(R.id.brand_recycler_view)
-    private lateinit var  recyclerView: RecyclerView
+    @Nullable
+    lateinit var  recyclerView: RecyclerView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_brand_display)
         ButterKnife.bind(this)
+        //recyclerView=findViewById(R.id.brand_recycler_view)
+        initRecycerView()
+    }
 
+    private fun initRecycerView() {
+        val layoutManger =LinearLayoutManager(this)
+        recyclerView.layoutManager=(layoutManger)
+        recyclerView.setHasFixedSize(true)
     }
 }
