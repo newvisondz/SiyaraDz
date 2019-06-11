f80c16ab4633ea6ef52b363a5e386bc053515c40
yacinebenkaidali <fy_benkaidali@esi.dz>
2019-06-11 23:32:30 +0100

    Switched ProfileForm to two-way databinding

--- app/src/main/java/com/newvisiondz/sayaradz/views/fragments/ProfileForm.kt
+++ app/src/main/java/com/newvisiondz/sayaradz/views/fragments/ProfileForm.kt
@@ -3,116 +3,47 @@ package com.newvisiondz.sayaradz.views.fragments
 import android.app.DatePickerDialog
 import android.content.Context
 import android.content.SharedPreferences
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
-import com.google.gson.JsonObject
 import com.newvisiondz.sayaradz.R
-import com.newvisiondz.sayaradz.services.RetrofitClient
-import com.newvisiondz.sayaradz.utils.getUserInfo
-import com.newvisiondz.sayaradz.utils.getUserToken
+import com.newvisiondz.sayaradz.databinding.FragmentProfileFormBinding
+import com.newvisiondz.sayaradz.views.viewModel.ProfileFormViewModel
+import com.newvisiondz.sayaradz.views.viewModel.ProfileFormViewModelFactory
 import kotlinx.android.synthetic.main.fragment_profile_form.*
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Response
 import java.util.*
 
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-class ProfileForm : androidx.fragment.app.Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
+class ProfileForm : Fragment() {
     private var userInfo: SharedPreferences? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-        userInfo = context!!.getSharedPreferences("userinfo", Context.MODE_PRIVATE)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile_form, container, false)
-    }
-
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val userInfoTmp = getUserInfo(userInfo!!)
-        user_date.setOnClickListener { datePicker() }
-        user_last_name.setText(userInfoTmp[0])
-        user_first_name.setText(userInfoTmp[3])
-        user_mail.text = userInfoTmp[2]
-
-        button_confirm.setOnClickListener {
-            progressForm.visibility = View.VISIBLE
-            val paramObject = JSONObject()
-            paramObject.put("firstName", user_first_name.text.toString())
-            paramObject.put("lastName", user_last_name.text.toString())
-            paramObject.put("address", user_adr.text.toString())
-            paramObject.put("phone", user_tel.text.toString())
-
-            val call = RetrofitClient(context!!)
-                .serverDataApi
-                .updateUser(getUserToken(userInfo!!)!!, paramObject.toString())
-
-            call.enqueue(object : retrofit2.Callback<JsonObject> {
-                override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
-                    if (response.isSuccessful) {
-                        progressForm.visibility = View.GONE
-                        val res: String = response.body()!!.get("ok").asString
-                        if (res == "1") {
-                            it.findNavController().navigate(R.id.action_profileForm_to_tabs)
-                        }
-                    }
-                }
-
-                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-                    t.printStackTrace()
-                }
-            })
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    interface OnFragmentInteractionListener {
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileForm().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        userInfo = context!!.getSharedPreferences("userinfo", Context.MODE_PRIVATE)
+        val binding: FragmentProfileFormBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_profile_form, container, false)
+        val application = requireNotNull(this.activity).application
+        val viewModelFactory = ProfileFormViewModelFactory(application)
+        val profileFormViewModel = ViewModelProviders.of(this, viewModelFactory).get(ProfileFormViewModel::class.java)
+        binding.lifecycleOwner = this
+        binding.viewModel = profileFormViewModel
+        profileFormViewModel.updateWithSuccess.observe(this, Observer {
+            if (it == true) {
+                view?.findNavController()?.navigate(ProfileFormDirections.actionProfileFormToTabs())
             }
+        })
+
+        binding.userDate.setOnClickListener { datePicker() }
+        return binding.root
     }
 
     private fun datePicker() {
@@ -120,8 +51,6 @@ class ProfileForm : androidx.fragment.app.Fragment() {
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
-
-
         val dpd = DatePickerDialog(
             activity as Context,
             DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
